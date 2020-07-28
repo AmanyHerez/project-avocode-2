35be13fcc337947af81f00d306f399adf3f33c11
AmanyHerez <amanyherez2000@gmail.com>
2020-07-28 14:56:08 +0300

    warranty policy

--- js/countdown.js
+++ js/countdown.js
@@ -1,17 +1,16 @@
-
-$(document).ready(function(){
+$( document ).ready(function() {
   setInterval(function time(){
-  var d =new Date();
-  var hours =24-d.getHours();
-  var min =60- d.getMinutes();
+  var d = new Date();
+  var hours = 24 - d.getHours();
+  var min = 60 - d.getMinutes();
   if((min + '').length == 1){
-    min ='0'+ min;
+    min = '0' + min;
   }
-  var sec=60-d.getSeconds();
-  if((sec+'').length==1){
-        sec ='0'+sec;
+  var sec = 60 - d.getSeconds();
+  if((sec + '').length == 1){
+        sec = '0' + sec;
   }
-  jQuery('#countdown#hour').html(hours);
-  jQuery('#countdown#min').html(min);
-  jQuery('#countdown#sec').html(sec);
-},1000);});
+  jQuery('#countdown #hour').html(hours);
+  jQuery('#countdown #min').html(min);
+  jQuery('#countdown #sec').html(sec);
+}, 1000); });
