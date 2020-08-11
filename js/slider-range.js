b33e3ab602815f7f1038475c3db6385f8d212c58
AmanyHerez <amanyherez2000@gmail.com>
2020-08-11 19:28:16 +0300

    slider range

--- js/slider-range.js
+++ js/slider-range.js
@@ -1,13 +1,25 @@
-$( function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 500,
-      values: [ 75, 300 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      }
+  $(document).ready(function() {
+
+  var doUpdate = function(event, ui) {
+    $('#slide1 .slide-back').remove();
+    $($('#slide1 a').get().reverse()).each(function(i) {
+      var bg = '#fff';
+      if (i == 1) {
+        bg = '#00f';
+      } else
+        bg = '#0f0';
+
+      $('#slide1').append($('<div></div>').addClass('slide-back').width($(this).offset().left - 3).css('background', bg));
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-  } );
+  };
+
+  $('#slide1').slider({
+    slide: doUpdate,
+    change: doUpdate,
+    min: 0,
+    max: 100,
+    values: [1, 50, 100]
+  });
+
+  doUpdate();
+});
